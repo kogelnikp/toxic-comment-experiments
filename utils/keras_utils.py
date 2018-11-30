217b6eed49c00186224b1514defadbcbb8c3a907
Unknown <philipp.kogelnik@hotmail.com>
2018-11-30 07:53:55 +0100

    performed 5 runs over simple cnn and calculated f1 score and roc auc

--- utils/keras_utils.py
+++ utils/keras_utils.py
@@ -1,9 +1,20 @@
-from sklearn.metrics import roc_auc_score
+import numpy as np
+from sklearn.metrics import roc_auc_score, f1_score
 from keras.callbacks import Callback
 
 class RocAucEvaluation(Callback):
     """Keras callback class for ROC AUC evaluation.
     Taken from: https://www.kaggle.com/yekenot/pooled-gru-fasttext    
+    The scores can be retrieved from self.scores where it is stored in the following format:
+          | ROC AUC | F1      |
+    Avg   |         |         |
+    STox  |         |         |
+    Tox   |         |         |
+    Obs   |         |         |
+    Thr   |         |         |
+    Ins   |         |         |
+    IdH   |         |         |
+
     """
 
     def __init__(self, validation_data=(), output_prefix='val', interval=1):
@@ -12,11 +23,27 @@ class RocAucEvaluation(Callback):
         self.interval = interval
         self.X_val, self.y_val = validation_data
         self.output_prefix = output_prefix
+        self.scores = []
 
     def on_epoch_end(self, epoch, logs={}):
         if epoch % self.interval == 0:
             y_pred = self.model.predict(self.X_val, verbose=0)
-            score = roc_auc_score(self.y_val, y_pred)
-            s_cl = roc_auc_score(self.y_val, y_pred, average=None)
-            print("\n {}: ROC-AUC - epoch: {:d} - score: {:.6f}".format(self.output_prefix, epoch+1, score))
-            print(" Tox: {} - STox: {} - Obs: {} - Thr: {} - Ins: {} - IdH: {}".format(s_cl[0], s_cl[1], s_cl[2], s_cl[3], s_cl[4], s_cl[5]))            
+            roc_avg = roc_auc_score(self.y_val, y_pred, average='micro')
+            roc_cl = roc_auc_score(self.y_val, y_pred, average=None)
+            f1_avg = f1_score(self.y_val, y_pred >= 0.5, average='micro')
+            f1_cl = f1_score(self.y_val, y_pred >= 0.5, average=None)
+
+            # print scores
+            print("\n {}: ROC-AUC - epoch: {:d} - score: {:.5f}".format(self.output_prefix, epoch+1, roc_avg))
+            print(" Tox: {:.5f} - STox: {:.5f} - Obs: {:.5f} - Thr: {:.5f} - Ins: {:.5f} - IdH: {:.5f}".format(roc_cl[0], roc_cl[1], roc_cl[2], roc_cl[3], roc_cl[4], roc_cl[5]))
+            print(" {}: F1 Score - epoch: {:d} - score: {:.5f}".format(self.output_prefix, epoch+1, f1_avg))
+            print(" Tox: {:.5f} - STox: {:.5f} - Obs: {:.5f} - Thr: {:.5f} - Ins: {:.5f} - IdH: {:.5f}".format(f1_cl[0], f1_cl[1], f1_cl[2], f1_cl[3], f1_cl[4], f1_cl[5]))
+
+            # save scores
+            epoch_scores = np.zeros((7, 2))
+            epoch_scores[0, 0] = roc_avg
+            epoch_scores[1:, 0] = roc_cl
+            epoch_scores[0, 1] = f1_avg
+            epoch_scores[1:, 1] = f1_cl
+            self.scores.append(epoch_scores)
+
--- utils/training_utils.py
+++ utils/training_utils.py
@@ -1,8 +1,11 @@
 import numpy as np
+import keras.backend as K
+import tensorflow as tf
 from sklearn.model_selection import KFold
 from sklearn.metrics import precision_recall_curve, f1_score, precision_score, recall_score
 from sklearn.metrics import average_precision_score
 from keras.callbacks import ModelCheckpoint
+from keras.models import clone_model
 from utils.keras_utils import RocAucEvaluation
 
 
@@ -41,3 +44,66 @@ def train_model(model, X, Y, validation_data, num_epochs, batch_size, optimizer,
     predictions = model.predict(validation_data[0], batch_size=batch_size)
 
     return model, predictions
+
+
+def train_and_evaluate_model(model_template, X, Y, validation_data, num_epochs, batch_size, optimizer, loss, metrics, weights_path=None, write_model_checkpoints=False, runs=10, seed=2018):
+    """trains and evaluates the given model for the specified number of runs
+    and returns the scores in the following format:
+    format: [run, train/val, epoch, class, metric]
+    train/val:  0..train, 1..val
+    class:      0..avg, 1-6..classes
+    metric:     0..rocauc, 1..f1
+    
+    Arguments:
+        model {keras model} -- the keras model to be evaluated
+        X {array} -- the tokenized comments
+        Y {array} -- the labels for each comment
+        validation_data {array} -- (X, y) data for validation
+        num_epochs {int} -- number of epochs per run
+        batch_size {int} -- the batch size
+        optimizer {string} -- optimizing algorithm
+        loss {string} -- loss function
+        metrics {array} -- metrics calculated after every epoch
+    
+    Keyword Arguments:
+        weights_path {string} -- path where the weights should be saved (default: {None})
+        write_model_checkpoints {bool} -- should the best model of each run be saved (default: {False})
+        runs {int} -- how many runs for evaluation (default: {10})
+        seed {int} -- seed for random variables (default: {2018})
+    
+    Returns:
+        numpy array -- train and test scores for each run and epoch
+    """
+
+    scores = np.zeros((runs, 2, num_epochs, 7, 2))
+
+    for run in range(runs):
+        print("RUN {}/{}".format(run+1, runs))
+        model = clone_model(model_template)
+        try:
+            model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+            callbacks = []
+            roc_callback_train = RocAucEvaluation((X, Y), output_prefix='train')
+            roc_callback_val = RocAucEvaluation(validation_data, output_prefix='val')
+            callbacks.append(roc_callback_train)
+            callbacks.append(roc_callback_val)
+
+            if write_model_checkpoints:
+                callbacks.append(ModelCheckpoint(weights_path, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='min'))
+            
+            model.fit(X, Y, validation_data=validation_data, epochs=num_epochs, batch_size=batch_size, \
+                callbacks=callbacks)
+            
+            scores[run, 0, :, :, :] = np.array(roc_callback_train.scores)
+            scores[run, 1, :, :, :] = np.array(roc_callback_val.scores)
+
+        finally:
+            del model
+            tf.reset_default_graph()
+            K.clear_session()
+
+    return scores
+        
+
+
